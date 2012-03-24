581cd9ec1eee7f5818465de5cbaf6b009a3b1599
Andrey Popp <8mayday@gmail.com>
2012-03-24 15:54:18 +0100

    wave API endpoint stub

--- fw.py
+++ fw.py
@@ -3,9 +3,12 @@
 from gevent import monkey
 monkey.patch_all()
 
+import collections
+
 from flask import Flask, render_template, request, abort
-from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
+from gevent.queue import Queue
+from geventwebsocket.handler import WebSocketHandler
 import werkzeug.serving
 
 import facebook
@@ -16,23 +19,35 @@ __all__ = ("app", "main")
 app = Flask(__name__)
 fb = facebook.API(settings.FB_APP_ID, settings.FB_SECRET)
 
+User = collections.namedtuple("User", ["id", "access_token"])
+
 @app.route("/")
 def welcome():
     return render_template("welcome.html", settings=settings)
 
-@app.route('/wave')
+@app.route("/wave")
 def wave():
-    if request.environ.get('wsgi.websocket'):
-        ws = request.environ['wsgi.websocket']
-        while True:
-            message = ws.wait()
-            ws.send(message)
-    return abort(400)
+    if not "wsgi.websocket" in request.environ:
+        return abort(400)
+    if not "id" in request.args or not "access_token" in request.args:
+        return abort(400)
+    u = User(request.args["id"], request.args["access_token"])
+    ws = request.environ["wsgi.websocket"]
+    for item in wave_for(u):
+        ws.send(item)
+
+def wave_for(u):
+    results = Queue()
+    def worker():
+        uinfo = fb.me.using(u.access_token).get()
+        ufinfo = fb.me.using(u.access_token).friends.get()
+        raise NotImplementedError()
+    return results
 
 @werkzeug.serving.run_with_reloader
 def main():
     app.debug = settings.DEBUG if hasattr(settings, "DEBUG") else False
-    http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+    http_server = WSGIServer(("", 5000), app, handler_class=WebSocketHandler)
     http_server.serve_forever()
 
 if __name__ == "__main__":
