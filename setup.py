30cc4ca2887592cea735731b95588a222f657c58
Andrey Popp <8mayday@gmail.com>
2012-03-24 13:05:35 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = "0.1"
+requires = []
+
+setup(
+    name="fw",
+    version=version,
+    description="FriendWave -- making mixes of your friends",
+    url="fw.zvooq.ru",
+    license="BSD",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    entry_points="""
+    # -*- Entry points: -*-
+    """)
